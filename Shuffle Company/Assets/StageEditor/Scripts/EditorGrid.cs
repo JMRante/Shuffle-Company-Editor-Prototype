0e1400f412285c0ddd4b89f1016f021ce67d96a1
Joshua Stark <joshuamstark@gmail.com>
2017-07-20 22:24:31 -0700

    Added script for behavior of the editor grid. Currently, the functions here are used to resize the grid based on input from the editor GUI.

---
+++ Shuffle Company/Assets/StageEditor/Scripts/EditorGrid.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorGrid : MonoBehaviour
+{
+    public Stage stage;
+    public EditorGridSelect editorGridSelect;
+
+    public void SetGridSizeX(string x)
+    {
+        stage.width = int.Parse(x);
+        UpdateEditorGrid();
+    }
+
+    public void SetGridSizeZ(string z)
+    {
+        stage.depth = int.Parse(z);
+        UpdateEditorGrid();
+    }
+
+    private void UpdateEditorGrid()
+    {
+        // Shift and resize editor grid to match new size
+        transform.localScale = new Vector3(stage.width, stage.depth, 1f);
+        transform.position = new Vector3(stage.width / 2f, transform.position.y, stage.depth / 2f);
+
+        // Set tiling of texture in shader to match new size
+        MeshRenderer meshRender = GetComponent<MeshRenderer>();
+        meshRender.material.SetVector("_MainTex_ST", new Vector4(stage.width, stage.depth, 0f, 0f));
+
+        // If needed, reset grid select position
+        if (editorGridSelect.GridX >= stage.width)
+        {
+            editorGridSelect.GridX = stage.width - 1;
+            editorGridSelect.UpdatePosition();
+        }
+
+        if (editorGridSelect.GridZ >= stage.depth)
+        {
+            editorGridSelect.GridZ = stage.depth - 1;
+            editorGridSelect.UpdatePosition();
+        }
+    }
+}
